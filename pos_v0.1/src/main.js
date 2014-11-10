72c6b47d43270832955b705baa128a231786214c
chansir1995 <1439925662@qq.com>
2014-11-10 14:18:30 +0800

    fix

--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -1,63 +1,9 @@
 //TODO: Please write code in this file.
-var inputs;
-inputs = [
-    {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
-
-    },
-    {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
-    },
-    {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
-    },
-    {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
-    },
-    {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
-    },
-    {
-        barcode: 'ITEM000001',
-        name: '雪碧',
-        unit: '瓶',
-        price: 3.00
-    },
-    {
-        barcode: 'ITEM000001',
-        name: '雪碧',
-        unit: '瓶',
-        price: 3.00
-    },
-    {
-        barcode: 'ITEM000004',
-        name: '电池',
-        unit: '个',
-        price: 2.00
-    }
-];
 function printInventory(inputs) {
     var item_num = inputs.length, i,j=0;
     var all = 0;
-    var tail="";
-    var item=inputs;
-
-    console.log('***<没钱赚商店>购物清单***\n');
+    var item = inputs;
+    var cont = '***<没钱赚商店>购物清单***\n';
     for (i = 0; i < item_num; i++){                                    //合并相同物品
         if (i == 0){item[j].barcode = inputs[i].barcode;
             item[j].name = inputs[i].name;
@@ -82,17 +28,13 @@ function printInventory(inputs) {
     }
     for(i = 0; i < j+1; i++) {                                          //打印
         var price = (item[i].count)*(item[i].price);
-        new_input = "名称：" + item[i].name + ",数量：" + item[i].count + item[i].unit+",单价：" + item[i].price + "(元)，小计：" + price + "(元)\n";
+        cont += "名称：" + item[i].name + "，数量：" + item[i].count + item[i].unit+"，单价：" + item[i].price.toFixed(2)  + "(元)，小计：" + price.toFixed(2) + "(元)\n";
         all += price;
-        console.log(new_input);
     }
-    tail= "----------------------\n" +
-    "总计："+all+"(元)\n"+
+    cont += "----------------------\n" +
+    "总计："+all.toFixed(2) +"(元)\n"+
     "**********************";
-    console.log(tail);
-
-
+    console.log(cont);
 
 }
 
-printInventory(inputs);
--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -1,42 +1,16 @@
 //TODO: Please write code in this file.
-var inputs;
-inputs = [
-    {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00,
-        count: 5
-    },
-    {
-        barcode: 'ITEM000001',
-        name: '雪碧',
-        unit: '瓶',
-        price: 3.00,
-        count: 2
-    },
-    {
-        barcode: 'ITEM000004',
-        name: '电池',
-        unit: '个',
-        price: 2.00,
-        count: 1
-    }
-];
 function printInventory(inputs) {
     var item_num = inputs.length, i;
-    var all = 0;
-    var tail="";
-    console.log('***<没钱赚商店>购物清单***\n');
+    var all = 0;  
+    var cont = '***<没钱赚商店>购物清单***\n'
     for (i = 0; i < item_num; i++) {
         var price = (inputs[i].count)*(inputs[i].price);
-            new_input = "名称：" + inputs[i].name + ",数量：" + inputs[i].count + inputs[i].unit+",单价：" + inputs[i].price + "(元)，小计：" + price + "(元)\n";
+            cont+= "名称：" + inputs[i].name + "，数量：" + inputs[i].count + inputs[i].unit+"，单价：" + inputs[i].price.toFixed(2) + "(元)，小计：" + price.toFixed(2) + "(元)\n";
         all += price;
-        console.log(new_input);
     }
-    tail= "----------------------\n" +
-    "总计："+all+"(元)\n"+
+    cont +=  "----------------------\n" +
+    "总计："+all.toFixed(2)+"(元)\n"+
     "**********************";
-    console.log(tail);
+    console.log(cont);
 }
-printInventory(inputs);
+
